be1b9d5303d90e16ff6fe5a60b508d883b3367f0
lihao <lihao@pconline.com.cn>
2020-03-22 16:02:29 +0800

    mapper

---
+++ src/main/java/com/csu/mall/dao/CategoryMapper.java
@@ -0,0 +1,17 @@
+package com.csu.mall.dao;
+
+import com.csu.mall.pojo.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+/**
+ * Created by lihao on 2020/3/22.
+ */
+@Mapper
+public interface CategoryMapper {
+
+    @Select("select * from mall_category where id = #{id}")
+    Category findById(@Param("id") Integer id);
+
+}
