dbcf093e5801159df7088a9fc8b61e5f319ae7ed
lihao <lihao@pconline.com.cn>
2020-03-22 16:27:24 +0800

    测试

--- src/test/java/com/csu/mall/MallApplicationTests.java
+++ src/test/java/com/csu/mall/MallApplicationTests.java
@@ -1,7 +1,10 @@
 package com.csu.mall;
 
+import com.csu.mall.dao.CategoryMapper;
+import com.csu.mall.pojo.Category;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +12,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class MallApplicationTests {
 
+	@Autowired
+	private CategoryMapper categoryMapper;
+
 	@Test
 	public void contextLoads() {
+		Category category = categoryMapper.findById(100001);
+		System.out.println(category.toString());
 	}
 
 }
