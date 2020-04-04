a7b08132884f4f6dda3ae434dad08976c0ede31c
lihao <lihao@pconline.com.cn>
2020-04-04 15:39:47 +0800

    继承基类

--- src/test/java/com/csu/mall/dao/CategoryMapperTest.java
+++ src/test/java/com/csu/mall/dao/CategoryMapperTest.java
@@ -1,19 +1,15 @@
 package com.csu.mall.dao;
 
+import com.csu.mall.MallApplicationTests;
 import com.csu.mall.pojo.Category;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 /**
  * Created by lihao on 2020/4/4.
  */
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class CategoryMapperTest {
+public class CategoryMapperTest extends MallApplicationTests {
     @Autowired
     private CategoryMapper categoryMapper;
 
