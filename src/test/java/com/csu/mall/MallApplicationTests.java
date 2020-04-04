7c6cbe64b1c100579a007315c496f1808069f443
lihao <lihao@pconline.com.cn>
2020-04-04 15:36:38 +0800

    作为基类

--- src/test/java/com/csu/mall/MallApplicationTests.java
+++ src/test/java/com/csu/mall/MallApplicationTests.java
@@ -1,30 +1,11 @@
 package com.csu.mall;
 
-import com.csu.mall.dao.CategoryMapper;
-import com.csu.mall.pojo.Category;
-import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class MallApplicationTests {
-
-	@Autowired
-	private CategoryMapper categoryMapper;
-
-	@Test
-	public void contextLoads() {
-		Category category = categoryMapper.findById(100001);
-		System.out.println(category.toString());
-	}
-
-	@Test
-	public void queryByIdTest() {
-		Category category = categoryMapper.queryById(100001);
-		System.out.println(category.toString());
-	}
 
 }
