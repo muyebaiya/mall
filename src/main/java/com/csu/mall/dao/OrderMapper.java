a63ae1afc1d6ac40b7111712ab94056ed778bd2d
lihao <lihao@pconline.com.cn>
2020-04-05 11:19:52 +0800

    订单mapper

---
+++ src/main/java/com/csu/mall/dao/OrderMapper.java
@@ -0,0 +1,17 @@
+package com.csu.mall.dao;
+
+import com.csu.mall.pojo.Order;
+
+public interface OrderMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Order record);
+
+    int insertSelective(Order record);
+
+    Order selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Order record);
+
+    int updateByPrimaryKey(Order record);
+}
